42c249c016ddbd73837cbc02d3a03e9929a20288
Vincent Zhang <ivincent.zhang@gmail.com>
2014-08-25 19:14:02 +0800

    add CategoryTree and WikiObjectModel which is required by product ontology

---
+++ extensions/WikiObjectModel/languages/Messages.php
@@ -0,0 +1,269 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'objecteditor' => 'Object Editor',
+	'wom_editor' => 'Object Model',
+	'wom-desc' => 'Easy Page Object Model for wiki user',
+);
+
+/** Message documentation (Message documentation)
+ * @author Beta16
+ */
+$messages['qqq'] = array(
+	'wom-desc' => '{{desc}}',
+);
+
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'objecteditor' => 'Objek-redigeerder',
+	'wom_editor' => 'Objek-model',
+	'wom-desc' => 'Maklike bladsyobjek-model',
+);
+
+/** Asturian (asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'objecteditor' => "Editor d'oxetos",
+	'wom_editor' => "Modelu d'oxetos",
+	'wom-desc' => "Modelu cenciellu d'oxetos de páxina pal usuariu de la wiki",
+);
+
+/** Belarusian (Taraškievica orthography) (‪беларуская (тарашкевіца)‬)
+ * @author Renessaince
+ */
+$messages['be-tarask'] = array(
+	'objecteditor' => 'Рэдактар аб’ектаў',
+	'wom_editor' => 'Мадэль аб’ектаў',
+	'wom-desc' => 'Мадэль аб’ектаў Easy Page для вікі-ўдзельніка',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'objecteditor' => 'Objekteditor',
+	'wom_editor' => 'Objektmodell',
+	'wom-desc' => 'Ermöglicht ein einfaches Objektmodell für Seiten',
+);
+
+/** Lower Sorbian (dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'objecteditor' => 'Objektowy editor',
+	'wom_editor' => 'Objektowy model',
+	'wom-desc' => 'Jadnory objektowy model za wikiwužywarja',
+);
+
+/** Spanish (español)
+ * @author Armando-Martin
+ * @author Bea.miau
+ */
+$messages['es'] = array(
+	'objecteditor' => 'Editor de objetos',
+	'wom_editor' => 'Modelo de objetos',
+	'wom-desc' => 'Modelo de objetos de página simple para los usuarios del wiki',
+);
+
+/** French (français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'objecteditor' => "Éditeur d'objet",
+	'wom_editor' => "Modèle d'objet",
+	'wom-desc' => "Modèle d'objet de page simple pour l'utilisateur wiki",
+);
+
+/** Franco-Provençal (arpetan)
+ * @author ChrisPtDe
+ */
+$messages['frp'] = array(
+	'objecteditor' => 'Èditor de chousa',
+	'wom_editor' => 'Modèlo de chousa',
+);
+
+/** Galician (galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'objecteditor' => 'Editor de obxectos',
+	'wom_editor' => 'Modelo de obxectos',
+	'wom-desc' => 'Modelo de obxectos de páxina sinxelo para os usuarios do wiki',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'objecteditor' => 'עורך עצמים',
+	'wom_editor' => 'דגם עצמים',
+	'wom-desc' => 'דגם עצמים קל לדף עבור משתמש ויקי',
+);
+
+/** Upper Sorbian (hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'objecteditor' => 'Objektowy editor',
+	'wom_editor' => 'Objektowy model',
+	'wom-desc' => 'Jednory objektowy model strony za wikiwužiwarja',
+);
+
+/** Interlingua (interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'objecteditor' => 'Editor de objectos',
+	'wom_editor' => 'Modello de objectos',
+	'wom-desc' => 'Modello facile de objectos de pagina pro usatores de wikis',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author Kenrick95
+ */
+$messages['id'] = array(
+	'objecteditor' => 'Editor Objek',
+	'wom_editor' => 'Model Objek',
+);
+
+/** Italian (italiano)
+ * @author Beta16
+ */
+$messages['it'] = array(
+	'objecteditor' => 'Editor oggetti',
+	'wom_editor' => 'Modello a oggetti',
+	'wom-desc' => 'Facile modello ad oggetti della pagina per utenti wiki',
+);
+
+/** Japanese (日本語)
+ * @author Schu
+ * @author Shirayuki
+ */
+$messages['ja'] = array(
+	'objecteditor' => 'オブジェクトエディター',
+	'wom_editor' => 'オブジェクトモデル',
+	'wom-desc' => 'ウィキ利用者のための簡単なページオブジェクトモデル',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'objecteditor' => 'Object Editeur',
+	'wom_editor' => 'Object-Schabloun',
+	'wom-desc' => 'Einfach Object-Schabloun vu Säite fir Wiki-Benotzer',
+);
+
+/** Macedonian (македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'objecteditor' => 'Уредник на објекти',
+	'wom_editor' => 'Модел на објект',
+	'wom-desc' => 'Лесен модел на објект за страници за корисник на вики',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'objecteditor' => 'Editor Objek',
+	'wom_editor' => 'Model Objek',
+	'wom-desc' => 'Model Objek Laman Mudah untuk pengguna wiki',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'objecteditor' => 'Objecttekstverwerker',
+	'wom_editor' => 'Objectmodel',
+	'wom-desc' => 'Gemakkelijk paginaobjectmodel',
+);
+
+/** Polish (polski)
+ * @author BeginaFelicysym
+ */
+$messages['pl'] = array(
+	'objecteditor' => 'Edytor obiektów',
+	'wom_editor' => 'Model obiektowy',
+	'wom-desc' => 'Prosty model obiektowy strony dla użytkownika wiki',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'objecteditor' => "Editor d'Oget",
+	'wom_editor' => "Model d'Oget",
+	'wom-desc' => "Model d'oget ëd pàgina sempia për l'utent wiki",
+);
+
+/** Portuguese (português)
+ * @author SandroHc
+ */
+$messages['pt'] = array(
+	'objecteditor' => 'Editor de Objectos',
+	'wom_editor' => 'Modelo de Objectos',
+	'wom-desc' => 'Modelo de Objectos "Easy Page" para os utilizadores da wiki',
+);
+
+/** Russian (русский)
+ * @author Express2000
+ */
+$messages['ru'] = array(
+	'objecteditor' => 'Редактор объектов',
+	'wom_editor' => 'Модель объекта',
+	'wom-desc' => 'Облегченная модель объекта страницы для вики-пользователя',
+);
+
+/** Swedish (svenska)
+ * @author Warrakkk
+ */
+$messages['sv'] = array(
+	'objecteditor' => 'Objektredigerare',
+	'wom_editor' => 'Objektmodell',
+	'wom-desc' => 'Enkel sidoobjektmodell för wikianvändare',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'objecteditor' => 'Patnugot ng Bagay',
+	'wom_editor' => 'Modelo ng Bagay',
+	'wom-desc' => 'Modelong Bagay ng Maginhawang Pahina para sa tagagamit ng wiki',
+);
+
+/** Turkish (Türkçe)
+ * @author Emperyan
+ */
+$messages['tr'] = array(
+	'objecteditor' => 'Nesne Düzenleyicisi',
+	'wom_editor' => 'Nesne Modeli',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'objecteditor' => 'Trình sửa đổi đối tượng',
+	'wom_editor' => 'Mô hình đối tượng',
+	'wom-desc' => 'Mô hình đối tượng trang dễ sử dụng trên wiki',
+);
+
+/** Simplified Chinese (‪中文（简体）‬)
+ * @author Linforest
+ */
+$messages['zh-hans'] = array(
+	'objecteditor' => '对象编辑器',
+	'wom_editor' => '对象模型',
+	'wom-desc' => '针对维基站点用户的简易页面对象模型',
+);
+
